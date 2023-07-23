ccd4f67299a4e7308e39012f8f96ecd2e58f4240
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-07-23 17:30:35 -0400

    fix: compile on linux

--- pty_linux.go
+++ pty_linux.go
@@ -5,10 +5,9 @@ package pty
 
 import (
 	"os"
+	"strconv"
 	"syscall"
 	"unsafe"
-
-	"golang.org/x/sys/unix"
 )
 
 func open() (*os.File, string, error) {
@@ -26,28 +25,28 @@ func open() (*os.File, string, error) {
 
 	slv, err := ptsname(m)
 	if err != nil {
-		return nil, nil, err
+		return nil, "", err
 	}
 
 	if err := unlockpt(m); err != nil {
-		return nil, nil, err
+		return nil, "", err
 	}
 
 	return m, slv, nil
 }
 
 func ptsname(f *os.File) (string, error) {
-	var n _C_uint
+	var n int32
 	_, _, e := syscall.Syscall(syscall.SYS_IOCTL, f.Fd(), syscall.TIOCGPTN, uintptr(unsafe.Pointer(&n)))
 	if e != 0 {
-		return e
+		return "", e
 	}
 
 	return "/dev/pts/" + strconv.Itoa(int(n)), nil
 }
 
 func unlockpt(f *os.File) error {
-	var u _C_int
+	var u int32
 	// use TIOCSPTLCK with a pointer to zero to clear the lock
 	_, _, e := syscall.Syscall(syscall.SYS_IOCTL, f.Fd(), syscall.TIOCSPTLCK, uintptr(unsafe.Pointer(&u))) //nolint:gosec // Expected unsafe pointer for Syscall call.
 	if e != 0 {
